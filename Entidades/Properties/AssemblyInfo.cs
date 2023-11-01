c1fea65b093c411dc362fb8178b3de3ea914bf20
charlichg <charlichg@gmail.com>
2023-11-01 09:16:52 -0600

    Cambio de capas

--- Entidades/Properties/AssemblyInfo.cs
+++ Entidades/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 
 // La información general de un ensamblado se controla mediante el siguiente 
 // conjunto de atributos. Cambie estos valores de atributo para modificar la información
-// asociada a un ensamblado.
+// asociada con un ensamblado.
 [assembly: AssemblyTitle("Entidades")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
-[assembly: Guid("7eb24b7c-5be1-4c19-bcdf-2b52bcc094de")]
+[assembly: Guid("58f7c0a4-68a6-48f4-ab19-88aef5812d8f")]
 
 // La información de versión de un ensamblado consta de los cuatro valores siguientes:
 //
